49f15a79eed84b1f072d751e344890427d42bcfe
shins777 <shins777@gmail.com>
2024-04-24 14:27:51 +0000

    update interpreter

---
+++ apps/interpreter/rag/constant.py
@@ -0,0 +1,20 @@
+
+# Logging level( INFO | DEBUG )
+logging = "DEBUG"
+
+# Constant 
+project_id = "ai-hangsik"
+region="asia-northeast3"
+
+#gemini_1_0_pro = "gemini-1.0-pro-001"
+gemini_1_0_pro = "gemini-1.5-pro-preview-0409"
+
+gemini_1_5_pro = "gemini-1.5-pro-preview-0409"
+
+#svc_acct_file = "/home/admin_/keys/ai-hangsik-71898c80c9a5.json"
+svc_acct_file = "/Users/hangsik/projects24/_service_account_key/ai-hangsik-71898c80c9a5.json"
+
+search_url = "https://discoveryengine.googleapis.com/v1alpha/projects/721521243942/locations/global/collections/default_collection/dataStores/kb-demo-app-ds_1710962510509/servingConfigs/default_search:search"
+
+# 검색 결과 개수
+num_search = 2
